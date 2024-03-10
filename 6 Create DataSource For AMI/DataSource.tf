63e76dbbaae8d965644fa5bf76c5a2d38803945b
unknown <sivakumr243@yahoo.co.in>
2024-03-10 05:40:27 +0530

    Modified DataSource.tf

--- 6 Create DataSource For AMI/DataSource.tf
+++ 6 Create DataSource For AMI/DataSource.tf
@@ -3,12 +3,14 @@
 data "aws_ami" "ec2ami" {
   //executable_users = ["self"]
   //most_recent      = true
-  owners           = ["137112412989"]
+  //owners           = ["137112412989"]
+  most_recent = true
+  //owners = ["self"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220805.0-x86_64-gp2"]
-    
+    //values = ["amzn2-ami-kernel-5.10-hvm-2.0.20220805.0-x86_64-gp2"]
+    values = ["amzn2-ami-kernel-5.1*"]
 
   }
 

809432231c1671b9c446a8b94a07d03b20659786
unknown <sivakumr243@yahoo.co.in>
2024-07-19 21:55:01 +0530

    Modified the existing repo

--- 1 Create EC2 With HardCoding/main.tf
+++ 1 Create EC2 With HardCoding/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "LinuxServerEC2" {
      
         ami ="ami-02eb7a4783e7e9317"
-        instance_type = "t2.micro"
+        instance_type = "t2.micro"  /* This is the instance Type */
         key_name = "SivaPemFile" 
         
         subnet_id = "subnet-05d1ae82f96d631ec"
